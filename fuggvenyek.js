ea31c284ebd9188965c142e68eba3f3a3de93597
SasGyula <gyulasass43@gmail.com>
2024-03-27 15:46:48 +0100

    db szamlalo

--- fuggvenyek.js
+++ fuggvenyek.js
@@ -1,6 +1,6 @@
-export function listaGeneral(){
-    const gombok = {};
-    for(let i = 0; i<9; i++){
+export function listaGeneral(n){
+    const gombok = [];
+    for(let i = 0; i<n*n; i++){
         gombok[i] = Math.floor(Math.random() * 2)
     }
     return gombok
@@ -8,11 +8,20 @@ export function listaGeneral(){
 
 export function jatekter(lista){
     let txt = "<div>"
-    for(let i = 0; i<9; i++){
+    for(let i = 0; i<lista.length; i++){
         txt += `<div class="gombok${lista[i]}"></div>`
     }
     txt += "</div>"
     return txt
+}
+export function hanyDarab(lista){
+    let db = 0;
+    for(let i = 0; i<lista.length; i++){
+        if (lista[i] > 0){
+            db += 1
+        }
+    }
+    return db
 }
 export function kapcsolok(event){
 
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-import { jatekter, kapcsolok, listaGeneral } from "./fuggvenyek.js";
+import { jatekter, kapcsolok, listaGeneral, hanyDarab } from "./fuggvenyek.js";
 
-const GOMBOK = listaGeneral();
+const GOMBOK = listaGeneral(3);
 const JATEKTER = $("#jatekter")
 JATEKTER.html(jatekter(GOMBOK))
 const UJJATEK = $(".ujJatek")
 UJJATEK.on("click", function(){
     location.reload()
 })
+console.log(GOMBOK.length)
+const PONTOK = $(".pontok")
+PONTOK.html(hanyDarab(GOMBOK))
