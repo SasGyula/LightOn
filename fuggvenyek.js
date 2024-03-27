c9da5b2546c080931f20727041810388ae641795
SasGyula <gyulasass43@gmail.com>
2024-03-27 15:08:23 +0100

    jatekter alap

---
+++ fuggvenyek.js
@@ -0,0 +1,8 @@
+export function jatekter(){
+    let txt = "<div>"
+    for(let i = 0; i<9; i++){
+        txt += `<div class="gombok"></div>`
+    }
+    txt += "</div>"
+    return txt
+}
---
+++ index.js
@@ -0,0 +1,4 @@
+import { jatekter } from "./fuggvenyek.js";
+
+const JATEKTER = $("#jatekter")
+JATEKTER.html(jatekter())
